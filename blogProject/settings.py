09599a480901d9064b1b24cc40f7ac10eb58952f
Durmuş PALA <71263028+durmus-pala@users.noreply.github.com>
2021-07-31 20:07:04 +0300

    add some codes for deploymnet to settings.py

--- blogProject/settings.py
+++ blogProject/settings.py
@@ -28,9 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+DISABLE_COLLECTSTATIC = 1
 
 
 # Application definition
